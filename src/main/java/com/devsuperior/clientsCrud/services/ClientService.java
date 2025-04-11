eaf905c098754982abf45f9a3fc5e7a974f4b0b2
murilopolli <murilopolli@celeparsl.smb4.parana>
2025-04-11 16:26:20 -0300

    Update and Delete

--- src/main/java/com/devsuperior/clientsCrud/services/ClientService.java
+++ src/main/java/com/devsuperior/clientsCrud/services/ClientService.java
@@ -1,15 +1,20 @@
 package com.devsuperior.clientsCrud.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.clientsCrud.dtos.ClientDTO;
 import com.devsuperior.clientsCrud.entities.Client;
 import com.devsuperior.clientsCrud.repositories.ClientRepository;
+import com.devsuperior.clientsCrud.services.exceptions.DataBaseException;
 import com.devsuperior.clientsCrud.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class ClientService {
@@ -37,8 +42,31 @@ public class ClientService {
 		c = repository.save(c);
 		return new ClientDTO(c);
 	}
+
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+		try {
+			Client c = repository.getReferenceById(id);
+			copyDtoToEntity(dto, c);		
+			c = repository.save(c);
+			return new ClientDTO(c);
+		} catch(EntityNotFoundException r) {
+			throw new ResourceNotFoundException("Recurso não encontrado");
+		}
+	}
 	
-	
+	@Transactional(propagation = Propagation.SUPPORTS)
+	public void delete(Long id) {
+		if (!repository.existsById(id)) {
+			throw new ResourceNotFoundException("Recurso não encontrado");
+		}
+		try {
+	       	repository.deleteById(id);    		
+		}catch (DataIntegrityViolationException e) {
+	       	throw new DataBaseException("Falha de integridade referencial");
+	   	}
+
+	}
 	
 	private void copyDtoToEntity(ClientDTO dto, Client c) {
 		c.setName(dto.getName());
