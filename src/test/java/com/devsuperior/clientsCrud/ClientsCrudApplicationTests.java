882ad85fe30f5251db28e4057c7e88d8e916da00
Murilo Polli <murilopolli@gmail.com>
2025-04-10 21:36:41 -0300

    first commit

---
+++ src/test/java/com/devsuperior/clientsCrud/ClientsCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.clientsCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientsCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
